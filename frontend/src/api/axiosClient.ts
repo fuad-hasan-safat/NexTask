8faad83ff3e047799495ae8faeb2939d51ef767c
Fuad Hasan <fuadsafat16@gmail.com>
2026-05-31 20:23:12 +0200

    update env variable

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: false
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  withCredentials: false,
 });
 
 axiosClient.interceptors.request.use((config) => {
